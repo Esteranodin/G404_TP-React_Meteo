0a495b6c1db7e9316bf48817f7ec3600b22b2fb6
esteranodin <esteranodin@hotmail.fr>
2025-03-21 12:25:33 +0100

    ADD: css custom

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -15,51 +15,44 @@ function Weather({ city, coordinates, dayData }) {
   if (!weatherData) return <div className='error-message'>Aucune donnée disponible</div>;
 
   // Pour récupérer les données pour aujourd'hui dans celles des prévisions (current -> forecast)
-  const todayForecast = weatherData.forecast && weatherData.forecast.length > 0 
-    ? weatherData.forecast.find(day => 
-        new Date(day.date).toDateString() === new Date().toDateString())
+  const todayForecast = weatherData.forecast && weatherData.forecast.length > 0
+    ? weatherData.forecast.find(day =>
+      new Date(day.date).toDateString() === new Date().toDateString())
     : null;
 
   // Données à afficher = jour sélectionné ou aujourd'hui
-  const displayData = dayData 
+  const displayData = dayData
     ? {
-        day: getDayOfWeek(dayData.date),
-        condition: dayData.day.condition.text,
-        icon: dayData.day.condition.icon,
-        temperature: Math.round(dayData.day.avgtemp_c),
-        maxtemp: Math.round(dayData.day.maxtemp_c),
-        mintemp: Math.round(dayData.day.mintemp_c),
-        wind: Math.round(dayData.day.maxwind_kph),
-        rain: dayData.day.daily_chance_of_rain,
-        sunrise: dayData.astro.sunrise,
-        sunset: dayData.astro.sunset,
-        moon: dayData.astro.moon_phase,
-      }
+      day: getDayOfWeek(dayData.date),
+      condition: dayData.day.condition.text,
+      icon: dayData.day.condition.icon,
+      temperature: Math.round(dayData.day.avgtemp_c),
+      maxtemp: Math.round(dayData.day.maxtemp_c),
+      mintemp: Math.round(dayData.day.mintemp_c),
+      wind: Math.round(dayData.day.maxwind_kph),
+      rain: dayData.day.daily_chance_of_rain,
+      sunrise: dayData.astro.sunrise,
+      sunset: dayData.astro.sunset,
+      moon: dayData.astro.moon_phase,
+    }
     : {
-        day: "Aujourd'hui",
-        condition: weatherData.current.condition.text,
-        icon: weatherData.current.condition.icon,
-        temperature: Math.round(weatherData.current.temp_c),
-        wind: weatherData.current.wind_kph,
-        sunrise: todayForecast?.astro?.sunrise,
-        sunset: todayForecast?.astro?.sunset,
-        moon: todayForecast?.astro?.moon_phase,
-        rain: todayForecast?.day?.daily_chance_of_rain,
-      };
+      day: "Aujourd'hui",
+      condition: weatherData.current.condition.text,
+      icon: weatherData.current.condition.icon,
+      temperature: Math.round(weatherData.current.temp_c),
+      wind: weatherData.current.wind_kph,
+      sunrise: todayForecast?.astro?.sunrise,
+      sunset: todayForecast?.astro?.sunset,
+      moon: todayForecast?.astro?.moon_phase,
+      rain: todayForecast?.day?.daily_chance_of_rain,
+    };
 
 
   return (
     <div className='weather card'>
       <div className='card-content'>
         <p className="day-label">{capitalizeFirstLetter(displayData.day)}</p>
-        
-        <h2 className="card-title">{weatherData.location.name}</h2>
-        
-        <p className="condition-text">{translateWeatherCondition(displayData.condition)}</p>
-        <p><img src={`https:${displayData.icon}`} alt="Weather icon" /></p>
-        
-        <p className="temperature">{displayData.temperature}°C</p>
-        
+
         <div className="astro-info">
           {displayData.sunrise && displayData.sunset && (
             <>
@@ -74,19 +67,23 @@ function Weather({ city, coordinates, dayData }) {
             </>
           )}
         </div>
-        
-        {displayData.moon && (
-          <p><img className="icon" src={moon} alt="icone de lune"/>{translateWeatherCondition(displayData.moon)}</p>
-        )}
-        
+        <h2 className="card-title">{weatherData.location.name}</h2>
+
+        <p className="condition-text">{translateWeatherCondition(displayData.condition)}</p>
+        <p><img src={`https:${displayData.icon}`} alt="Weather icon" /></p>
+
+        <p className="temperature">{displayData.temperature}°C</p>
+
         <div className="weather-details">
           <p>Vent: {displayData.wind} km/h</p>
           {displayData.rain !== undefined && (
             <p>Probabilité de pluie: {displayData.rain}%</p>
           )}
         </div>
-        
 
+        {displayData.moon && (
+          <p><img className="icon" src={moon} alt="icone de lune" />{translateWeatherCondition(displayData.moon)}</p>
+        )}
       </div>
     </div>
   );
